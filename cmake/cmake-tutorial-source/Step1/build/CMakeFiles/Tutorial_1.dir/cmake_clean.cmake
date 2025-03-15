f2cf80222bc4b0683f00c3bacffa3fc5ed352597
FrankEastwood <475559574@qq.com>
2025-03-15 09:29:13 +0800

    add cmake and csapp

---
+++ cmake/cmake-tutorial-source/Step1/build/CMakeFiles/Tutorial_1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tutorial_1.dir/tutorial.cxx.o"
+  "CMakeFiles/Tutorial_1.dir/tutorial.cxx.o.d"
+  "Tutorial_1"
+  "Tutorial_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tutorial_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
